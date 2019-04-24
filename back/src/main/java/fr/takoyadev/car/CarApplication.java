0622c18e7d234532f7cbcd290bd9ea3e5ed369e4
Christophe EIMARD <ceimard@klanik.com>
2019-04-24 09:22:28 +0200

    MAJ pour faire tourner le projet sur un Tomcat externe

--- back/src/main/java/fr/takoyadev/car/CarApplication.java
+++ back/src/main/java/fr/takoyadev/car/CarApplication.java
@@ -2,10 +2,16 @@ package fr.takoyadev.car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CarApplication {
+public class CarApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CarApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CarApplication.class, args);
 	}
