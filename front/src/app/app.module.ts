4c217804abdd535c57f3c2017f288109d1863efb
chinjto <cyril_defaye@yahoo.fr>
2020-11-17 01:29:21 +0100

    #12 Création de l'écran d'édition pour une voiture
    Ajout modif' d'une voiture + Ajout détail voiture + Ajout détail entretien

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,30 +1,30 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { AuthComponent } from './auth/auth.component';
-import { RouterModule, Routes } from '@angular/router';
-import { AuthService } from './services/auth.service';
-import { FourOhFourComponent } from './erreur/404/four-oh-four.component';
-import { AuthGuard } from './services/auth-guard.service';
-import { DetailEntretienComponent } from './entretien/detail/detail-entretien.component';
-import { AddEntretienComponent } from './entretien/add/add-entretien.component';
-import { ListEntretienComponent } from './entretien/list/list-entretien.component';
-import { ItemEntretienComponent } from './entretien/item/item-entretien.component';
-import { ListVoitureComponent } from './voiture/list/list-voiture.component';
-import { DetailVoitureComponent } from './voiture/detail/detail-voiture.component';
-import { ItemVoitureComponent } from './voiture/item/item-voiture.component';
-import { ProprietaireService } from './services/proprietaire.service';
-import { VoitureService } from './services/voiture.service';
-import { EntretienService } from './services/entretien.service';
-import { ListProprietaireComponent } from './proprietaire/list/list-proprietaire.component';
-import { DetailProprietaireComponent } from './proprietaire/detail/detail-proprietaire.component';
-import { ItemProprietaireComponent } from './proprietaire/item/item-proprietaire.component';
-import { CarEasterEggComponent } from './easter-egg/car/car-easter-egg.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {AuthorizationInterceptor} from "./interceptor/http.interceptor";
-import {AngularMyDatePickerModule} from "angular-mydatepicker";
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {AuthComponent} from './auth/auth.component';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthService} from './services/auth.service';
+import {FourOhFourComponent} from './erreur/404/four-oh-four.component';
+import {AuthGuard} from './services/auth-guard.service';
+import {DetailEntretienComponent} from './entretien/detail/detail-entretien.component';
+import {AddEntretienComponent} from './entretien/add/add-entretien.component';
+import {ListEntretienComponent} from './entretien/list/list-entretien.component';
+import {ItemEntretienComponent} from './entretien/item/item-entretien.component';
+import {ListVoitureComponent} from './voiture/list/list-voiture.component';
+import {DetailVoitureComponent} from './voiture/detail/detail-voiture.component';
+import {ItemVoitureComponent} from './voiture/item/item-voiture.component';
+import {ProprietaireService} from './services/proprietaire.service';
+import {VoitureService} from './services/voiture.service';
+import {EntretienService} from './services/entretien.service';
+import {ListProprietaireComponent} from './proprietaire/list/list-proprietaire.component';
+import {DetailProprietaireComponent} from './proprietaire/detail/detail-proprietaire.component';
+import {ItemProprietaireComponent} from './proprietaire/item/item-proprietaire.component';
+import {CarEasterEggComponent} from './easter-egg/car/car-easter-egg.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {AuthorizationInterceptor} from './interceptor/http.interceptor';
+import {AngularMyDatePickerModule} from 'angular-mydatepicker';
 import {AddVoitureComponent} from './voiture/add/add-voiture.component';
 import {AddProprietaireComponent} from './proprietaire/add/add-proprietaire.component';
 
@@ -33,6 +33,7 @@ const appRoutes: Routes = [
   { path: 'entretiens/add', canActivate: [AuthGuard], component: AddEntretienComponent },
   { path: 'entretiens/:id', canActivate: [AuthGuard], component: DetailEntretienComponent },
   { path: 'voitures', canActivate: [AuthGuard], component: ListVoitureComponent },
+  { path: 'voitures/edit/:id', canActivate: [AuthGuard], component: AddVoitureComponent },
   { path: 'voitures/add', canActivate: [AuthGuard], component: AddVoitureComponent },
   { path: 'voitures/:id', canActivate: [AuthGuard], component: DetailVoitureComponent },
   { path: 'proprietaires', canActivate: [AuthGuard], component: ListProprietaireComponent },
--- front/src/app/voiture/add/add-voiture.component.ts
+++ front/src/app/voiture/add/add-voiture.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {NgForm} from '@angular/forms';
 import {VoitureService} from '../../services/voiture.service';
 import {Proprietaire} from '../../proprietaire/proprietaire';
@@ -15,25 +15,50 @@ export class AddVoitureComponent implements OnInit {
 
   errorMsg = '';
   proprietaires: Proprietaire[];
+  voiture;
+  id: number;
+  addModeActivate: boolean;
 
   @ViewChild('f') form: NgForm;
 
-  constructor(private proprietaireService: ProprietaireService, private voitureService: VoitureService, private router: Router) { }
+  constructor(
+    private proprietaireService: ProprietaireService,
+    private voitureService: VoitureService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
     this.proprietaireService.getList().subscribe(data => this.proprietaires = data);
+    this.id = this.route.snapshot.params.id;
+    this.addModeActivate = true;
+    this.voitureService.getById(+this.id).subscribe(
+      data => {
+        this.voiture = data;
+        if (data != null) {
+          this.addModeActivate = false;
+          this.form.setValue({
+            proprietaire: this.voiture.owner.id,
+            marque:       this.voiture.marque,
+            modele:       this.voiture.modele
+          });
+        } else {
+          this.addModeActivate = true;
+        }
+      },
+        error => console.log(error)
+    );
   }
 
   saveVoiture() {
     const proprietaire = new Proprietaire();
     proprietaire.id = this.form.value.proprietaire;
-    const voiture = new Voiture();
-    voiture.owner = proprietaire;
-    voiture.marque = this.form.value.marque;
-    voiture.modele = this.form.value.modele;
-    this.voitureService.save(voiture);
-
-    this.voitureService.save(voiture).subscribe(() => this.router.navigate(['/..']), error => {
+    this.voiture = new Voiture();
+    this.voiture.id = this.id;
+    this.voiture.owner = proprietaire;
+    this.voiture.marque = this.form.value.marque;
+    this.voiture.modele = this.form.value.modele;
+    this.voitureService.save(this.voiture).subscribe(() => this.router.navigate(['/voitures']), error => {
       console.log(error);
       this.errorMsg = 'Remplis correctement ton formulaire, fumier !!!';
     });
